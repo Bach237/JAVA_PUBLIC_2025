9113831f050970ed26199845dfc2395b2fea3d36
Bach237 <ndbach669@gmail.com>
2025-07-07 22:54:57 +0700

    Sua bai 1

--- bai1/src/bai1.java
+++ bai1/src/bai1.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-public class Main {
+public class bai1 {
 
-    public static boolean ktra(int n){
+    public static boolean kiemTra(int n){
         for(int i = 2; i <= Math.sqrt(n); i++){
             if(n % i == 0) return false;
         }
@@ -12,15 +12,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int[] a = new int[n];
         for(int i = 0; i < n; i++){
             a[i] = sc.nextInt();
         }
-
         for(int x : a){
-            System.out.print(x + " ");
+            if(kiemTra(x)) System.out.println("Day la so nguyen to");
+            else System.out.println("Day khong la so nguyen to");
         }
-
     }
 }
