68d8601d6ea441734f894b2565e02387cba34add
Bach237 <ndbach669@gmail.com>
2025-07-07 19:51:31 +0700

    Bai 1 sua lan 3

--- bai1/src/Main.java
+++ bai1/src/Main.java
@@ -1,7 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+
+public class Helloword {
+
+    public static boolean ktra(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0) return false;
+        }
+        return n > 1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Heloo word!ssdasdhfvasdjf");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        for(int x : a){
+            System.out.print(x + " ");
+        }
+
     }
 }
