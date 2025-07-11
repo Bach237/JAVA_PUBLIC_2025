6896f2ac6777fc2a1fe7cee0822f54877529a760
Bach237 <ndbach669@gmail.com>
2025-07-12 01:24:34 +0700

    Bai trong wed: https://claoj.edu.vn/problem/cookies

--- BachNguyen/src/Bai1.java
+++ BachNguyen/src/Bai1.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Bai1 {
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        int[] Min = new int[n + 1];
+        int[] Sum = new int[n + 1];
+        Min[n] = a[n];
+        Sum[n] = a[n];
+        for(int i = n - 1; i >= 1; i--){
+            Min[i] = Math.min(a[i], Min[i + 1]);
+            Sum[i] = a[i] + Sum[i + 1];
+        }
+
+        int Max = 0;
+        int[] k = new int[n - 1];
+        for(int i = 1; i <= n - 2; i++){
+            int res = (Sum[i + 1] - Min[i + 1]) / (n - (i + 1));
+            Max = Math.max(Max, res);
+            k[i] = res;
+        }
+        for(int i = 1; i <= n - 2; i++){
+            if(k[i] == Max){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
