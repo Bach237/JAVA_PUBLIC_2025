47436fa92664f983d1bae2bc792f1f87527ad24f
Bach237 <ndbach669@gmail.com>
2025-07-18 22:22:53 +0700

    Xoá thư mục BT_Java_CLB

--- BT_Java_CLB/src/P2_Bai2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class P2_Bai2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double r = sc.nextDouble();
-
-    }
-}
